220f7e27c7fbb87894ddc4830c4fcfbef42d46c2
Carsten Nielsen <carsten@carsten-mbp.local>
2008-03-27 01:22:10 -0400

    Created a test for the domain resolver.  I will now see if my theory of using one instance of Resolv::DNS to stop the too many files open error is true.

---
+++ test/unit/domain_resolver_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+
+class DomainResolverStressTest < Test::Unit::TestCase
+  
+  def test_five_hundred_consecutive_queries
+    flunk 'This should be fun...'
+  end
+  
+end
